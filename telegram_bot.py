ad7a4db5ed99aee2847f3e8c212e04eaf901c07e
NOOBGLITCH <64962222+NOOBGLITCH@users.noreply.github.com>
2025-07-21 23:45:42 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,20 +1,19 @@
 # telegram_bot.py
 import telegram
-from telegram.constants import ParseMode # Corrected import for ParseMode
+from telegram.constants import ParseMode
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
 
-def send_telegram_message(message):
-    # Handle multiple chat IDs if provided as a comma-separated string
+async def send_telegram_message(message):
     chat_ids = [chat_id.strip() for chat_id in TELEGRAM_CHAT_ID.split(',')]
 
     for chat_id in chat_ids:
         try:
-            bot.send_message(
+            await bot.send_message( # Added 'await' here
                 chat_id=chat_id,
                 text=message,
-                parse_mode=ParseMode.MARKDOWN, # Corrected usage of ParseMode
+                parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=False
             )
             print(f"Telegram message sent successfully to chat ID: {chat_id}.")
